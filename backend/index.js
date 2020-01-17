2ee932435e6ff5fdcc07bf370f05dc1e18b8e7c5
Thiago Garcia Tamosauskas <thiago.tamosauskas@umesp.edu.dti>
2020-01-17 19:13:45 -0300

    metodos e parametros

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,30 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // Método HTTP: GET, POST, PUT, DELETE
 
 // Tipos de parâmetros
-// Query Params:
-// Route Params:
-// Body:
+// Query Params: request.query (Filtros, ordenação, paginação, etc...)
+// Route Params: request.params (identificar um recurso para alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de registro)
 
-app.post('/', (request, response) => {
-return response.json({ message:'Hellosauskas GT' });
+app.post('/users', (request, response) => {
+    console.log(request.body);
 });
+
+// app.get('/users', (request, response) => {
+//    return response.json({ message:'Hellosauskas GT' });
+//});
+
+//app.query('/users', (request, response) => {
+//    console.log(request.params)
+//});
+
+// app.delete('/users/:id', (request, response) => {
+// console.log(request.params)
+//    return response.json({ message:'Hellosauskas GT' });
+//});
 
 app.listen(3333);
