fb01d1ffafd0abefbb761ffd9d2c650b9eff2300
Thiago Garcia Tamosauskas <thiago.tamosauskas@umesp.edu.dti>
2020-01-23 16:23:11 -0300

    organização dos packeges

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://Tamosauskas:tesla@1996@cluster0-zwotg.gcp.mongod
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,6 @@
 // import React, {useState} from 'react'; 
-import React, { useEffect } from 'react'; 
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
 
 import './global.css';
 import './App.css';
@@ -7,10 +8,19 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() { 
+
+    const [github_username, setGithubUsername] = useState('');    
+    const [techs, setTechs] = useState('');
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setLongitude] = useState('');
+
 useEffect(() =>{
     navigator.geolocation.getCurrentPosition(
         (position)=>{
-            console.log(position);
+            const {latitude, longitude} = position.coords;
+
+            setLatitude(latitude);
+            setLongitude(longitude);
         },
         (err) => {
             console.log(err);
@@ -21,33 +31,68 @@ useEffect(() =>{
     )
 }, []);
     
+async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+        github_username,
+        techs,
+        latitude,
+        longitude,
+    } )
+    console.log(response.data);
+};
 
     return(
 
     <div id="app">
     <aside>
         <strong >Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
 
             <div className="input-block">
             <label htmlFor="github_username">Usuário do GitbHub</label>
-            <input name="github_username" id="github_username" required />
+            <input 
+            name="github_username" 
+            id="github_username" 
+            required
+            value={github_username}
+            onChange={e => setGithubUsername(e.target.value)}
+
+            />
             </div>
             <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input 
+            name="techs" 
+            id="techs" 
+            required 
+            value={techs}
+            onChange={e => setTechs(e.target.value)} />
             </div>
             <div className="input-block">
             <div className="input-group">
             
             <div className="input-block">
                 <label htmlFor="latitude">Latitude</label>
-                <input name="latitude" id="latitude" required />
+                <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)} />
             </div>
 
              <div className="input-block">
                 <label htmlFor="longitude">Longitude</label>
-                <input name="longitude" id="longitude" required />
+                <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                required 
+                value={longitude} 
+                onChange={e => setLongitude(e.target.value)}/>
             </div>
 
             </div>
