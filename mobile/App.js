783a3b7cabf57210e8c2ea25bda50c577fbaa0e9
erickfirmo <erickluques@gmail.com>
2020-01-27 19:10:51 -0300

    add: reactive native dependencies

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello World!</Text>
     </View>
   );
 }
@@ -12,8 +12,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
+    fontWeight: "bold",
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 32,
+    color:'#fff',
   },
 });
