7301073c84f568cf60258e1e8a11577c52a7d711
TTamosauskas <thiago.gt@gmail.com>
2020-01-28 11:51:06 -0300

    feat: adding Main and Profiles routes

--- mobile/App.js
+++ mobile/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
-    </View>
+    <>
+    <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
+    <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    fontWeight: "bold",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontWeight: "bold",
-    fontSize: 32,
-    color:'#fff',
-  },
-});
